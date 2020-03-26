c39d8f75c8d351d39ded3bb5e416f637a8e7a0bb
john <physitime@gmail.com>
2020-03-27 04:12:30 +0430

    set provider to localhost

--- pweb3/index.js
+++ pweb3/index.js
@@ -1,7 +1,7 @@
 
 const Web3 = require('pweb3'); 
 
-const web3 = new Web3('http://46.4.199.148:6969/pchain');
+const web3 = new Web3('http://localhost:6969/pchain');
 
 function getBlockNumber(){
     return web3.pi.getBlockNumber(); 
